abe51432a1514641c2c6ec2ba310832f011ee131
Takahiro Nago <takahiro.nago.art@gmail.com>
2023-05-18 01:20:44 +0900

    composer.json added

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 
 	<!-- google font -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
 
 	<!-- stylesheet -->
